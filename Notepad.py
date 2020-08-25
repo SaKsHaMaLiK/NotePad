da7953b58bb9fb169967ce700ceffd0547102fa0
Saksham Malik <54699504+SaKsHaMaLiK@users.noreply.github.com>
2020-08-25 10:54:42 +0530

    Add files via upload

---
+++ Notepad.py
@@ -0,0 +1,21 @@
+import tkinter import *
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+from tkinter import filedialog
+from tkinter import messagebox, font
+from tkinter import ttk
+from datetime import datetime
+import webbrowser
+
+
+menubar = Menu(root)
+
+file = Menu(meubar, tearoff=0)
+file.add_command(label="New", command=new)
+file.add_command(label="Newwindow", command=new_window)
+file.add_command(label="Open", command=Open)
+file.add_command(label="Save", command=save)
+file.add_command(label="SaveAs", command=save_as)
+file.add_separator()
+file.add_command(label="Exit", command=exit)
+menubar.add_cascade(label="file", menu=file, font=('verdana', 10, 'bold'))
