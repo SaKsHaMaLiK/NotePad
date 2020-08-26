f89b8b818f7215b76d0cff46a0d05cd7ffd9086e
Saksham Malik <54699504+SaKsHaMaLiK@users.noreply.github.com>
2020-08-26 10:33:41 +0530

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -7,20 +7,25 @@ from tkinter import ttk
 from datetime import datetime
 import webbrowser
 
-root = tk.Tk()
-root.geometry('500x500')
-root.title('notepad')
-root.iconbitmap('notepad.ico')
+
+def new():
+    text.delete('1.0', 'end')
 
 
-menubar = Menu(root)
+def new_window():
+    root = tk.Tk()
+    root.geometry('500x500')
+    root.title('notepad')
+    root.iconbitmap('notepad.ico')
 
-file = Menu(meubar, tearoff=0)
-file.add_command(label="New", command=new)
-file.add_command(label="Newwindow", command=new_window)
-file.add_command(label="Open", command=Open)
-file.add_command(label="Save", command=save)
-file.add_command(label="SaveAs", command=save_as)
-file.add_separator()
-file.add_command(label="Exit", command=exit)
-menubar.add_cascade(label="file", menu=file, font=('verdana', 10, 'bold'))
+    menubar = Menu(root)
+
+    file = Menu(meubar, tearoff=0)
+    file.add_command(label="New", command=new)
+    file.add_command(label="Newwindow", command=new_window)
+    file.add_command(label="Open", command=Open)
+    file.add_command(label="Save", command=save)
+    file.add_command(label="SaveAs", command=save_as)
+    file.add_separator()
+    file.add_command(label="Exit", command=exit)
+    menubar.add_cascade(label="file", menu=file, font=('verdana', 10, 'bold'))
